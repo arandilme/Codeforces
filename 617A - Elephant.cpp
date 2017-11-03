39d135f0cc19344fbfc5723964d739c3b19589b3
Athena <arandilme@users.noreply.github.com>
2017-11-03 21:35:35 +0300

    Add files via upload

---
+++ 617A - Elephant.cpp
@@ -0,0 +1,19 @@
+/*
+ * 617A - Elephant.cpp
+ * http://codeforces.com/problemset/problem/617/A
+ *  Created on: Nov 3, 2017
+ *      Author: athena
+ * 32034184 	617A - 11 	GNU C++14 	Accepted 	15 ms 	8 KB 	2017-11-03 21:34:40 	2017-11-03 21:34:40
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin >> x;
+
+	cout << (x + 4) / 5 << endl;
+
+	return 0;
+}
