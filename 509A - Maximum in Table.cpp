bc92ecea3b76aad3af4dcbf210b27426de5283f7
Athena <athena@yandex.ru>
2018-01-26 23:40:23 +0300

    Add files via upload

---
+++ 509A - Maximum in Table.cpp
@@ -0,0 +1,21 @@
+/*
+ * 509A - Maximum in Table.cpp
+ * http://codeforces.com/problemset/problem/509/A
+ *  Created on: Jan 26, 2018
+ *      Author: athena
+ * 34558545 	509A - 18 	GNU C++14 	Accepted 	15 ms 	1852 KB 	2018-01-26 00:31:12 	2018-01-26 00:31:12
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int arr[11] = {0, 1, 2, 6, 20, 70, 252, 924, 3432, 12870, 48620};
+
+	cout << arr[n] << endl;
+
+	return 0;
+}
