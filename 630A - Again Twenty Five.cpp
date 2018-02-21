0a6399a228847b81f01fe4dfdc8b10c1bf958328
Athena <athena@yandex.ru>
2018-02-21 23:01:02 +0300

    Add files via upload

---
+++ 630A - Again Twenty Five.cpp
@@ -0,0 +1,19 @@
+/*
+ * 630A - Again Twenty Five.cpp
+ * http://codeforces.com/problemset/problem/630/A
+ *  Created on: Feb 21, 2018
+ *      Author: athena
+ * 35558551 	630A - 21 	GNU C++14 	Accepted 	15 ms 	1848 KB 	2018-02-21 23:00:13 	2018-02-21 23:00:13
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	cout << "25" << endl;
+
+	return 0;
+}
